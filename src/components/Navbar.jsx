ead99c6c237ae6a00a4c9cc6f3728e19767b515c
aman2712 <khanamankhan1973@gmail.com>
2024-09-14 09:35:48 +0200

    first pushc

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar container' >
+        <a href="" id="brand">Lyon</a>
+        <div className="menu">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
